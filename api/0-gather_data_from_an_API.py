60e9c1e890390f8747c9cff17c9587f5d80ae432
semaed <figsaned@gmail.com>
2023-08-21 19:37:03 -0400

    Correction

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,45 +1,50 @@
 #!/usr/bin/python3
-"""Script to return information of todo list"""
-import requests
-import sys
+"""
+Method to given employee ID,
+returns information about his/her TODO list progress
+"""
+from requests import get
+from sys import argv
 
 
-def fetch_employee_todo_list(employee_id):
-    # Base URL for the JSONPlaceholder API
-    base_url = "https://jsonplaceholder.typicode.com"
+def information_employee():
+    """
+    Returns information about employees
+    """
+    id_employee = int(argv[1])
+    id_employee = int(argv[1])
+    employee_name = ""
+    number_of_done_task = 0
+    total_number_of_task = 0
+    task_title = []
 
-    # URLs for fetching user data and TODO list for the given employee ID
-    user_url = f"{base_url}/users/{employee_id}"
-    todos_url = f"{base_url}/todos?userId={employee_id}"
+    url_users = 'https://jsonplaceholder.typicode.com/users'
+    url_todos = 'https://jsonplaceholder.typicode.com/todos'
 
-    # Fetch user data and TODO list from the API
-    response_user = requests.get(user_url)
-    response_todos = requests.get(todos_url)
+    response_one = get(url_users)
+    response_two = get(url_todos)
 
-    # Extract JSON data from the responses
-    user_data = response_user.json()
-    todos_data = response_todos.json()
+    if response_one.status_code == 200:
+        response_json_usr = response_one.json()
+        response_json_tod = response_two.json()
 
-    # Extract employee name and calculate completed tasks
-    employee_name = user_data.get("name")
-    total_tasks = len(todos_data)
-    completed_tasks = [task for task in todos_data if task["completed"]]
+        for user in response_json_usr:
+            if (user['id'] == id_employee):
+                employee_name = user['name']
 
-    # Display TODO list progress information
-    print(
-        f"Employee {employee_name} is done with tasks({len(completed_tasks)}/{total_tasks}):")
-    for task in completed_tasks:
-        print(f"\t{task['title']}")
+                for tod in response_json_tod:
+                    if tod['userId'] == id_employee:
+                        total_number_of_task += 1
+                        if tod['completed'] is True:
+                            number_of_done_task += 1
+                            task_title.append(tod['title'])
+
+        print('Employee {} is done with tasks({}/{}):'
+              .format(employee_name, number_of_done_task,
+                      total_number_of_task))
+        for title in task_title:
+            print('\t {}'.format(title))
 
 
 if __name__ == "__main__":
-    # Check for the correct number of command-line arguments
-    if len(sys.argv) != 2:
-        print("Usage: python 0-gather_data_from_an_API.py employee_id")
-        sys.exit(1)
-
-    # Parse the employee ID from the command-line argument
-    employee_id = int(sys.argv[1])
-
-    # Fetch and display the TODO list progress for the specified employee ID
-    fetch_employee_todo_list(employee_id)
+    information_employee()
